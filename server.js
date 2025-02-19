1be302a18b2e9aa1c50059b535f75ec9b79a826c
Arnie Ungvary <djusa1221@gmail.com>
2025-02-19 17:34:49 +0100

    Added PAT for CI/CD pipeline 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! GHHAHHAccccccccHA');
+  res.send('Hello World! GHHAHHAccccccaaaaccHA');
 });
 
 app.listen(port, () => {
